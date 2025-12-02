fa7d83a5afc6f377a7b39f1030f60d1c74f3f83f
Rualin <fateevrual@gmail.com>
2025-12-02 17:51:15 +0700

    refactor: add ex3

---
+++ ex03/delayed_turtle/launch/turtle_tf2_demo_launch.py
@@ -0,0 +1,50 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    target_name_arg = DeclareLaunchArgument(
+        'target_frame', default_value='turtle1',
+        description='Target frame name.',
+    )
+    delay_arg = DeclareLaunchArgument(
+        "delay", default_value="5.0",
+        description="Delay for turtle2 running in seconds",
+    )
+    return LaunchDescription([
+        target_name_arg,
+        delay_arg,
+        Node(
+            package='turtlesim',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        Node(
+            package='delayed_turtle',
+            executable='turtle_tf2_broadcaster',
+            name='broadcaster1',
+            parameters=[
+                {'turtlename': 'turtle1'}
+            ]
+        ),
+        Node(
+            package='delayed_turtle',
+            executable='turtle_tf2_broadcaster',
+            name='broadcaster2',
+            parameters=[
+                {'turtlename': 'turtle2'}
+            ]
+        ),
+        Node(
+            package='delayed_turtle',
+            executable='turtle_tf2_listener',
+            name='listener',
+            parameters=[
+                {'target_frame': LaunchConfiguration('target_frame')},
+                {"delay": LaunchConfiguration("delay")},
+            ]
+        ),
+    ])
